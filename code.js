13ef70980aeb63ba71ce4e73cfce38250dd7393f
Lukas Heini <lukas.heini.98@googlemail.com>
2025-06-17 00:14:51 +0200

    initial commit

---
+++ code.js
@@ -0,0 +1,114 @@
+const ROW_COUNT = 6;
+const COL_COUNT = 5;
+let currentRow = 0;
+let currentCol = 0;
+let isEntered = false;
+let interval;
+
+const firstRow = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
+const secondRow = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
+const thirdRow = ["Enter", "Z", "X", "C", "V", "B", "U", "N", "M", "Delete"];
+const rowItems = [firstRow, secondRow, thirdRow];
+
+function createMap() {
+    const container = document.querySelector('.board-container');
+
+    for(let i = 0; i < ROW_COUNT; i++){
+        const rowContainer = document.createElement('div');
+        rowContainer.classList.add('row-item');
+        rowContainer.id = `row-${i}`;
+        for(let j = 0; j < COL_COUNT; j++){
+            const fieldItem = document.createElement('div');
+            fieldItem.id = `${i},${j}`;
+            fieldItem.classList.add('field-item');
+            rowContainer.append(fieldItem);
+        }
+        container.append(rowContainer);
+    }
+}
+
+function createKeyboard() {
+    const container = document.querySelector('.keyboard-container');
+    const ROW_COUNT = 3;
+
+    for(let i = 0; i < ROW_COUNT; i++){
+        const rowContainer = document.createElement('div');
+        rowContainer.classList.add('row-item');
+        const items = rowItems[i];
+        for(let j = 0; j < items.length; j++){
+            const fieldItem = document.createElement('div');
+            fieldItem.classList.add('keyboard-item');
+            fieldItem.innerHTML = items[j];
+            fieldItem.addEventListener("click", () => {
+                addLetter(fieldItem.innerHTML);
+            });
+            rowContainer.append(fieldItem);
+        }
+        container.append(rowContainer);
+    }
+}
+
+function processKeypress(key) {
+    const keyUppercase = key.toUpperCase();
+    const elements = firstRow.concat(secondRow, thirdRow);
+    if (elements.includes(keyUppercase) || elements.includes(key)){
+        if(key == "Enter"){
+            if(currentCol == 4){
+                isEntered = true;
+            }
+            animateLetterBackground();
+            return;
+        }
+        addLetter(keyUppercase);
+    }
+    else {
+        return;
+    }
+}
+
+function addLetter(letterValue) {
+    const id = currentRow + "," + currentCol;
+    const element = document.getElementById(id);
+    element.innerHTML = letterValue;
+    element.classList.add('scale-up-center');
+    if(currentCol == 4){
+        return;
+    }
+    if(currentCol == 4 && isEntered){
+        currentCol = 0;
+        currentRow++;
+    }
+    else{
+        currentCol++;
+    }
+    element.addEventListener("animationend", () => {
+        element.classList.remove('scale-up-center');
+    });
+}
+
+function animateLetterBackground() {
+    const elements = document.querySelectorAll('#row-' + currentRow)[0].childNodes;
+    let i = 0;
+    interval = setInterval(() => {
+        animateGrid(elements, i);
+        i++;
+    }, 100);
+}
+
+function animateGrid(elements, index) {
+    if(index < elements.length){
+        elements[index].classList.add('bg-animate-correct-letter');
+    }
+    else {
+        clearInterval(interval);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    createMap();
+    createKeyboard();
+});
+
+document.addEventListener("keydown", (event) => {
+    processKeypress(event.key);
+});
