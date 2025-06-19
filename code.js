dd289fe270f55dac3945b082327255c63d4430be
Lukas Heini <lukas.heini.98@googlemail.com>
2025-06-19 15:41:55 +0200

    removed bugs and added deterministic word selection

--- code.js
+++ code.js
@@ -6,7 +6,9 @@ let isEntered = false;
 let interval;
 let isAnimationDone = true;
 let correctLetters = 0;
-const word = "party";
+let enteredWord = "";
+let wordToGuess = "";
+let isWordGuessed = false;
 
 const firstRow = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
 const secondRow = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
@@ -28,6 +30,11 @@ function createMap() {
         }
         container.append(rowContainer);
     }
+}
+
+async function wordExists(word) {
+    const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
+    return response.ok;
 }
 
 function createKeyboard() {
@@ -63,6 +70,7 @@ function processKeypress(key) {
     const keyUppercase = key.toUpperCase();
     const elements = firstRow.concat(secondRow, thirdRow);
     if (elements.includes(keyUppercase) || elements.includes(key)){
+        console.log(key)
         if(key == "Enter"){
             if(currentCol == 4){
                 isEntered = true;
@@ -78,16 +86,28 @@ function processKeypress(key) {
     }
 }
 
-function addLetter(letterValue) {
+function countCorrectLetters(word1, word2) {
+    let lettersMatched = 0;
+    for(let i = 0; i < word1.length; i++){
+        if(word1[i] === word2[i]){
+            lettersMatched++;
+        }
+    }
+    return lettersMatched;
+}
+
+async function addLetter(letterValue) {
     const id = currentRow + "," + currentCol;
     const element = document.getElementById(id);
     if(isAnimationDone){
         element.innerHTML = letterValue;
         element.classList.add('scale-up-center');
         if(currentCol == 4 && isEntered){
-            currentCol = 0;
-            currentRow++;
-            isEntered = !isEntered;
+            if(await wordExists(enteredWord)){
+                // currentRow++;
+                // currentCol = 0;
+                isEntered = !isEntered;
+            }
         }
         else if(currentCol < 4){
             currentCol++;
@@ -97,12 +117,25 @@ function addLetter(letterValue) {
         element.classList.remove('scale-up-center');
         setTimeout(() => {
             isAnimationDone = true;
+            let enteredWord = "";
+            for (let i = 0; i < COL_COUNT; i++){
+                const id = currentRow + "," + i;
+                const element = document.getElementById(id);
+                enteredWord += element.innerHTML;
+            }
+            correctLetters = countCorrectLetters(enteredWord.toLowerCase(), wordToGuess);
             if(correctLetters == 5){
-                alert("Win!");
+                isWordGuessed = true;
+                console.log("win")
             }
             correctLetters = 0;
-        }, 3000);
+        }, 2000);
     });
+}
+
+async function getFiveLetterWords() {
+    const text = await fetch("words.txt");
+    return text.text();
 }
 
 function removeLetter() {
@@ -114,7 +147,31 @@ function removeLetter() {
     }
 }
 
-function animateLetterBackground() {
+async function animateLetterBackground() {
+    enteredWord = "";
+    const rowElements = [];
+    for (let i = 0; i < COL_COUNT; i++){
+        const id = currentRow + "," + i;
+        const element = document.getElementById(id);
+        enteredWord += element.innerHTML;
+        rowElements.push(element);
+    }
+    if(await wordExists(enteredWord)){
+        setTimeout(() => {
+            currentCol = 0;
+            currentRow++;
+        }, 1000);
+    }
+    else{
+        for (let j = 0; j < rowElements.length; j++){
+            const rowElement = rowElements[j];
+            rowElement.classList.add('wobble-hor-bottom');
+            rowElement.addEventListener("animationend", () => {
+                rowElement.classList.remove('wobble-hor-bottom');
+            });
+        }
+        return;
+    }
     const elements = document.querySelectorAll('#row-' + currentRow)[0].childNodes;
     let i = 0;
     interval = setInterval(() => {
@@ -124,10 +181,10 @@ function animateLetterBackground() {
 }
 
 function checkLetter(enteredLetter, indexInWord) {
-    if(word[indexInWord] == enteredLetter.toLowerCase()){
+    if(wordToGuess[indexInWord] == enteredLetter.toLowerCase()){
         return "bg-animate-correct-position";
     }
-    else if(word[indexInWord] !== enteredLetter.toLowerCase() && word.includes(enteredLetter.toLowerCase())){
+    else if(wordToGuess[indexInWord] !== enteredLetter.toLowerCase() && wordToGuess.includes(enteredLetter.toLowerCase())){
         return "bg-animate-correct-letter";
     }
     else {
@@ -138,9 +195,6 @@ function checkLetter(enteredLetter, indexInWord) {
 function animateGrid(elements, index) {
     if(index < elements.length){
         const cssClass = checkLetter(elements[index].innerHTML, elements[index].id.split(",")[1]);
-        if(cssClass == "bg-animate-correct-position"){
-            correctLetters++;
-        }
         elements[index].classList.add(cssClass);
     }
     else {
@@ -151,10 +205,19 @@ function animateGrid(elements, index) {
 document.addEventListener("DOMContentLoaded", () => {
     createMap();
     createKeyboard();
+    getFiveLetterWords().then((text) => {
+        const lines = text.split("\n");
+        const startDate = new Date(2024, 0, 1);
+        const today = new Date();
+        const diffTime = today.setHours(0,0,0,0) - startDate.setHours(0,0,0,0);
+        const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
+
+        wordToGuess = lines[diffDays % lines.length].replace("\r", "");
+    });
 });
 
 document.addEventListener("keydown", (event) => {
-    if(isAnimationDone){
+    if(isAnimationDone && !isWordGuessed){
         if(event.key === 'Backspace'){
             removeLetter();
         }
